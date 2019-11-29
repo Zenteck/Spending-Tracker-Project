889f6ce200275e4263048095ebb2f1dc3f0ccca2
Alan <spambox+github@zenteck.co.uk>
2019-11-29 10:29:52 +0000

    setup files & sql

---
+++ db/spending_tracker.sql
@@ -0,0 +1,20 @@
+DROP TABLE transanctions;
+DROP TABLE tags;
+DROP TABLE merchants;
+
+CREATE TABLE merchants (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE transactions (
+  id SERIAL PRIMARY KEY,
+  merchant_id INT REFERENCES merchants(id),
+  tag_id INT REFERENCES tags(id),
+  amount DECIMAL(18, 2)
+)
