3a1376ade55700deb244d00a1852953b09d103fb
Alan <spambox+github@zenteck.co.uk>
2019-11-29 10:54:20 +0000

    merchant CRUD

---
+++ models/merchants.rb
@@ -0,0 +1,49 @@
+require_relative('../db/sql_runner')
+
+class Merchant
+
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(info)
+    @id = info['id'].to_i() if options['id']
+    @name = info['name']
+  end
+
+#Which needs brackets with a single value? this or update?
+#CREATE
+  def save()
+    sql = "INSERT into merchants
+    (name) VALUES ($1)
+    RETURNING id"
+    values = [@name]
+    result = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i()
+  end
+
+#READ
+  def self.all()
+    sql = "SELECT * FROM merchants"
+    merchants = SqlRunner.run(sql)
+    return merchants.map{|merchant| Merchant.new(merchant)}
+  end
+
+#UPDATE
+  def update()
+    sql = "UPDATE merchants SET (name) = (s1) WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+#DELETE
+  def delete()
+    sql = "DELETE FROM merchants WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELTE FROM merchants"
+    SqlRunner.run(sql)
+  end
+end
